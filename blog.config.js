c059ad74727298991836f8b7ec40eb0fc6bb1878
ClaireLind <87566424+ClaireLind1@users.noreply.github.com>
2025-01-04 15:43:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Active Reading', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'ClaireLind', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://activereadings.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 英语, English, Language Learning', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
